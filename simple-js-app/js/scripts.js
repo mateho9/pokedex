cae58ac60d778e05e2a65e150393c2e6d0c4c2a1
mateho9 <88411689+mateho9@users.noreply.github.com>
2021-12-06 12:12:05 -0600

    Added first pokemon array
    Deleted all code previously in file and added 4 pokemon in my pokemonList array. Data was pulled from pokedex.org

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello World');
-let favoriteFood = 'spaghetti';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Nidoran', height: 0.4, types: ['poison']},
+    {name: 'Magikarp', height: 1, types: ['water']},
+    {name: 'Evee', height: 0.3, types: ['normal']},
+]
+
 //remember to use PEMDAS on math expressions
