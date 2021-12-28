371ffb1ebf40236f7454f26db49d816dd0122c24
mateho9 <88411689+mateho9@users.noreply.github.com>
2021-12-28 10:35:48 -0600

    Added loop to display data
    Added a loop to display the information on certain pokemon

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,8 +1,19 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Nidoran', height: 0.4, types: ['poison']},
-    {name: 'Magikarp', height: 1, types: ['water']},
-    {name: 'Evee', height: 0.3, types: ['normal']},
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Nidoran', height: 0.4, type: ['poison']},
+    {name: 'Magikarp', height: 1, type: ['water']},
+    {name: 'Evee', height: 0.3, type: ['normal']},
 ]
-
 //remember to use PEMDAS on math expressions
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " " + "(height: "
+    + pokemonList[i].height + ", type: " + pokemonList[i].type + ") ");
+    if(pokemonList[i].height >0.9){
+        //this will display a certain message for big pokemon
+        document.write("Wow, thats one big Pokemon!");
+    }
+    document.write('<br>');
+}
+// condition is i < pokemonList.length
+// action is i++
