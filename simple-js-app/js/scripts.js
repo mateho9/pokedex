e16b671c064aefec124239919a75462e72c81f07
mateho9 <88411689+mateho9@users.noreply.github.com>
2022-01-19 10:19:34 -0600

    forEach and IIFE addition
    I removed the for and replaced it with a forEach. I also moved the pokemon list under an IIFE

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,19 +1,41 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    {name: 'Nidoran', height: 0.4, type: ['poison']},
-    {name: 'Magikarp', height: 1, type: ['water']},
-    {name: 'Evee', height: 0.3, type: ['normal']},
-]
-//remember to use PEMDAS on math expressions
-
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + " " + "(height: "
-    + pokemonList[i].height + ", type: " + pokemonList[i].type + ") ");
-    if(pokemonList[i].height >0.9){
-        //this will display a certain message for big pokemon
-        document.write("Wow, thats one big Pokemon!");
+let pokemonRepository = (function () {
+    //pokemon array
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Nidoran', 
+            height: 0.4, 
+            type: ['poison']
+        },
+        {
+            name: 'Magikarp', 
+            height: 1, 
+            type: ['water']
+        },
+        {
+            name: 'Evee', 
+            height: 0.3, 
+            type: ['normal']
+        },
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-    document.write('<br>');
-}
-// condition is i < pokemonList.length
-// action is i++
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+    };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + 'M tall and ' + pokemon.type + ' type.');
+});
