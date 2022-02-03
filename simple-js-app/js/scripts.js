4e8fffb43ea861aa60fe3fe3e4f94abe191b5c2f
mateho9 <88411689+mateho9@users.noreply.github.com>
2022-02-02 18:36:14 -0600

    Drew helped me upload through cmd

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonRepository = (function () {
     //pokemon array
+    let modalContainer = document.querySelector('#modal-container');
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     //function to add pokemon to the list
@@ -12,11 +13,72 @@ let pokemonRepository = (function () {
     }
     //used to execute loadDetails inside the console
     function showDetails(pokemon) {
-        console.log(pokemon.name);
         loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+            showModal(pokemon.name, 'Height: ' + pokemon.height. pokemon.imageUrl);
         });
     }
+
+    //function to show the modal and its details
+    function showModal(title, text, img_src) {
+      let modalCotainer = document.querySelector('#modal-container');
+
+    //clear all previous content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    //add the content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+
+    let modalTitle = document.createElement('h1');
+    modalTitle.innerText = title;
+
+    let modalText = document.createElement('p');
+    modalText.innerText = text;
+
+    let modalImg = document.createElement('img');
+    modalImg.classList.add('modal-img');
+    // setting `src` property to set the actual element's `src` attribute
+    // this also works on <img> elements selected by querySelector() method, 
+    // it is not specific for <img> elements created with createElement() methods
+    modalImg.src = img_src;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(modalTitle);
+    modal.appendChild(modalText);
+    modal.appendChild(modalImg);
+    modalContainer.appendChild(modal);
+
+    //made modal visible
+    modalContainer.classList.add('is-visible');
+
+    //close modal with esc button
+    closeButtonElement.addEventListener('click', hideModal);
+
+    //close modal when you click outside the box
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if(target === modalContainer) {
+        hideModal;
+      }
+    });
+  }
+
+    function hideModal() {
+      modalContainer.classList.remove('is-visible');
+    }
+
+    // close modal with esc
+    window.addEventListener('keydown', (e) => {
+      let modalContainer = document.querySelector('#modal-container');
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
+        hideModal();
+      }
+    });
+
 
     //added spacing so its easier to read for now
     function addListItem(pokemon) {
@@ -34,6 +96,13 @@ let pokemonRepository = (function () {
             console.log(event);
         });
     }   
+
+    //event listener to show pokemon details when you click on the box
+    function buttonEventListener(button, pokemon){
+      button.addEventListener('click', function(){
+        showDetails(pokemon);
+      });
+    }
 
     function loadList() {
         return fetch(apiUrl).then(function (response) {
